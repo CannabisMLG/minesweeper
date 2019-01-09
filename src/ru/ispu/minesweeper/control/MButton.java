f212153bea7b838e8a292b91fe46a0bd8a83bce5
Alex <cugfug576@gmail.com>
2019-01-09 23:51:35 +0300

    Косметические изменения

--- src/ru/ispu/minesweeper/control/MButton.java
+++ src/ru/ispu/minesweeper/control/MButton.java
@@ -1,6 +1,8 @@
 package ru.ispu.minesweeper.control;
 
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class MButton extends Button {
 	private int x, y;
@@ -17,6 +19,23 @@ public class MButton extends Button {
 		flag = !flag;
 		if(flag) this.setText("?");
 		else this.setText("");
+	}
+
+	public void setCText(String s, int val)
+	{
+		this.setText(s);
+		this.setStyle("-fx-font-weight: bold");
+		switch (val)
+		{
+			case 1: this.setTextFill(Color.BLUE); break;
+			case 2: this.setTextFill(Color.GREEN); break;
+			case 3: this.setTextFill(Color.RED); break;
+			case 4: this.setTextFill(Color.web("#000066")); break;
+			case 5: this.setTextFill(Color.web("#330000")); break;
+			case 6: this.setTextFill(Color.web("#330066")); break;
+			case 7: this.setTextFill(Color.web("#006600")); break;
+			case 8: this.setTextFill(Color.web("#333333")); break;
+		}
 	}
 	
 	public boolean isFlag()
